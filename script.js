229f128cde0d2d3bb1c18df58c2554eef3eb9b65
Gilberto <gilbertoruiz28@gmail.com>
2022-09-21 03:31:26 -0300

    buttons links

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+//buttons selection
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operationb]')
+const equalsButtons = document.querySelector('[data-equals]')
+const deleteButtons = document.querySelector('[data-delete]')
+const allClearButtons = document.querySelector('[data-all-clear]')
+const previousOperationTextElement = document.querySelector('[data-previous-operand]')
+const currentOperationTextElement = document.querySelector('[data-current-operand]')
